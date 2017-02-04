e573fca13c2c0469af6362417a4aae1489c4ad12
fanfansama <fberthault@gmail.com>
2017-02-05 00:13:17 +0100

    add docker feature / missing discovery integration

---
+++ hystrix-dashboard/src/main/java/Application.java
@@ -0,0 +1,18 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@SpringCloudApplication
+@EnableHystrixDashboard
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	@RequestMapping("/")
+	public String home() {
+		return "forward:/hystrix";
+	}
+}
