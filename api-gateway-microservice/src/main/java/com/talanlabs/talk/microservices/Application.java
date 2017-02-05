77c671eabf7a651b65264728c523126fe9c77ff5
fanfansama <fberthault@gmail.com>
2017-02-05 18:41:26 +0100

    add docker & docker-compose

--- api-gateway-microservice/src/main/java/com/talanlabs/talk/microservices/Application.java
+++ api-gateway-microservice/src/main/java/com/talanlabs/talk/microservices/Application.java
@@ -2,10 +2,11 @@ package com.talanlabs.talk.microservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.cloud.netflix.zuul.EnableZuulServer;
 
 @SpringCloudApplication
-@EnableZuulServer
+@EnableZuulProxy
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
